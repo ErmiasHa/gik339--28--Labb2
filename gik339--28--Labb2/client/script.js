5c7b8af69f3493e8e15bfa2cf80266f417fa7215
ErmiashS <117041999+ErmiashS@users.noreply.github.com>
2023-12-19 22:56:23 +0100

    fri commit

---
+++ gik339--28--Labb2/client/script.js
@@ -0,0 +1,53 @@
+console.log("Script.js is linked!");
+
+document.addEventListener('DOMContentLoaded', fetchData);
+
+
+async function fetchData() {
+  try {
+    const response = await fetch('http://localhost:3000/users');
+    const data = await response.json();
+    console.log(data); 
+
+
+
+    // Anropa renderUsers med användardata
+    renderUsers(data);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+}
+
+
+function renderUsers(users) {
+    const userList = document.getElementById('userList');
+  
+    users.forEach(user => {
+      const listItem = document.createElement('li');
+      const nameParagraph = document.createElement('p');
+      const usernameParagraph = document.createElement('p');
+      
+      // innerText för att undvika HTML-injektion
+      nameParagraph.innerText = `${user.firstName} ${user.lastName}`;
+      usernameParagraph.innerText = user.username;
+  
+      // namn och användarnamn i listelementet
+      listItem.appendChild(nameParagraph);
+      listItem.appendChild(usernameParagraph);
+  
+      //CSS-stil 
+      listItem.style.margin = '2px';
+    
+     
+    listItem.style.backgroundColor = user.color;
+      userList.appendChild(listItem);
+
+
+    });
+  }
+
+
+
+  
+  
+  
---
+++ gik339--28--Labb2/server/server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const server = express();
+
+server
+  .use(express.json())
+  .use(express.urlencoded({ extended: false }))
+  .use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    res.header('Access-Control-Allow-Methods', '*');
+    next();
+  });
+
+
+
+  server.get('/users', (req, res) => {
+    db.all('SELECT * FROM users', (err, rows) => {
+      if (err) {
+        // Lägg till felhantering om det behövs
+        res.status(500).send(err);
+      } else {
+        res.send(rows);
+      }
+    });
+  });
+  
+
+  const PORT = 3000;
+  server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+
+
+  
+  
+
+  const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./gik339-labb2.db');
+
+
+
+
